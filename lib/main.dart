70268ff89b0a60dfc39a14841efa23e6f18aebf3
rifat <rifat@tikweb.dk>
2024-05-14 19:45:21 +0600

    notification service is implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quick_chat/constants/app_theme.dart';
+import 'package:quick_chat/feature/home_screen/home_screen.dart';
+import 'package:quick_chat/feature/login_screen/login_screen.dart';
 import 'package:quick_chat/helper/firebase_options.dart';
-import 'package:quick_chat/ui/home_screen/home_screen.dart';
-import 'package:quick_chat/ui/login_screen/login_screen.dart';
-import 'package:quick_chat/ui/sign_up_screen/signup_screen.dart';
+import 'package:quick_chat/network/notification_service.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
+// const AndroidNotificationChannel channel = AndroidNotificationChannel(
+//     'high_importance_channel', // id
+//     'High Importance Notifications', // title
+//     description: 'This channel is used for important notifications.', // description
+//     importance: Importance.high,
+//     playSound: true);
+// final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+//
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   print('A bg message just showed up :  ${message.messageId}');
+// }
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(ProviderScope(child: const MyApp()));
+  NotificationService().initNotification();
+/*  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);*/
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  // FirebaseNotificationService.instance.initialize();
+  tz.initializeTimeZones();
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -23,13 +53,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: theme(),
-      home: FirebaseAuth.instance.currentUser != null ? HomeScreen() : LoginScreen(),
-
+      home: FirebaseAuth.instance.currentUser != null ? const HomeScreen() : LoginScreen(),
     );
   }
 }
-
--- lib/network/chat_service.dart
+++ lib/network/chat_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:quick_chat/constants/app_assets.dart';
 import 'package:quick_chat/model/message.dart';
@@ -47,4 +48,42 @@ class ChatService extends ChangeNotifier {
       messageController.clear();
     }
   }
+  /*Future<void> sendNotification(String receiverID, String message) async {
+    // Get the receiver's FCM token from Firestore or any other source
+    // Assuming you have a function to get the FCM token based on the receiver's ID
+    String receiverFCMToken = await getReceiverFCMToken(receiverID);
+
+    // Prepare the notification message
+    var notificationMessage = {
+      'notification': {
+        'title': 'New Message',
+        'body': message,
+      },
+      'token': receiverFCMToken,
+    };
+
+    // Send the notification using Firebase Cloud Messaging
+    await FirebaseMessaging.instance.send(notificationMessage);
+  }
+  Future<String> getReceiverFCMToken(String receiverID) async {
+    try {
+      // Access Firestore and get the document for the receiver using their ID
+      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('user').doc(receiverID).get();
+
+      // Check if the user exists
+      if (userSnapshot.exists) {
+        // Get the FCM token from the user document
+        String fcmToken = userSnapshot['fcm'];
+        return fcmToken;
+      } else {
+        // Handle case where user does not exist
+        print('User with ID $receiverID does not exist.');
+        return '';
+      }
+    } catch (e) {
+      // Handle any errors that may occur during the process
+      print('Error retrieving FCM token: $e');
+      return '';
+    }
+  }*/
 }
